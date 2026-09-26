b9e6fe29a38b03f102bab3a7031052b2277ab6a4
AllisonE-code <allisonmeigsti@gmail.com>
2026-09-26 13:15:39 +0200

    removed vercel.json

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,5 @@
 require('./config/env');
+
 const app = require('./app');
-const { connectToDatabase } = require('./config/db');
-
-// 1. Create a middleware that ensures the DB is connected before handling the request
-app.use(async (req, res, next) => {
-  try {
-    await connectToDatabase();
-    next();
-  } catch (error) {
-    console.error('Database connection failed:', error.message);
-    res.status(500).json({ error: 'Internal Server Error (Database)' });
-  }
-});
-
 
 module.exports = app;
